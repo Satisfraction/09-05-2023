0d2e6102070a898e5792b3853564acae390c1ce7
Universatorium <matthias.bandholtz@docc.techstarter.de>
2023-05-09 23:42:26 +0200

    final

--- testjack.js
+++ testjack.js
@@ -9,7 +9,7 @@ var karte = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Bube', 'Dame', 'König', 'Ass'];
 const farben = ['♥', '♦', '♣', '♠'];
 let deck = [];
 
-// Erstelle das Deck mit allen möglichen Kombinationen aus Werten und Farben
+//Erstelle das Deck mit allen möglichen Kombinationen aus Werten und Farben
 for (let i = 0; i < farben.length; i++) {
   for (let j = 0; j < karte.length; j++) {
     const wert = karte[j];
@@ -23,7 +23,16 @@ function getRandomIndex() {
 }
 
 // Ziehe zufällig 4 Karten aus dem Deck
-function game() {
+function game(){
+
+  // for (let i = 0; i < farben.length; i++) { //Alternative für endloses Spielen mit immer neuem Deck
+  //   for (let j = 0; j < karte.length; j++) {
+  //     const wert = karte[j];
+  //     const mappedWert = werteMapping[wert] || wert;
+  //     deck.push(farben[i] + ' ' + mappedWert);
+  //   }
+  // }
+
   const hand1 = [];
   for (let i = 0; i < 2; i++) {
     const randomIndex = getRandomIndex();
@@ -66,72 +75,76 @@ function game() {
     "Die Karten des Computers sind: " + hand1 + "<br><br>" +
     "Deine Punktzahl: " + hand2Sum + "<br><br>" +
     "Punktzahl des Computers: " + hand1Sum + "<br><br>";
+  // if (deck = []) {
+  //   ergebnisEl2.innerHTML += "Das Deck ist leer";
+  // }
   if (hand2Sum > hand1Sum) {
     ergebnisEl2.innerHTML += "Du hast gewonnen!";
   } else if (hand1Sum > hand2Sum) {
     ergebnisEl2.innerHTML += "Der Computer hat gewonnen";
-  } else {
+  } else if (hand1Sum === hand2Sum) {
     ergebnisEl2.innerHTML += "Unentschieden!"
-  }
-// Beim Laden der Seite das Array aus dem localStorage laden
-let ergebnisse = JSON.parse(localStorage.getItem('ergebnisse')) || [];
-// Set variable in local storage
-// localStorage.setItem('ergebnisse', 'variableValue');
+  } 
+    // Beim Laden der Seite das Array aus dem localStorage laden
+    let ergebnisse = JSON.parse(localStorage.getItem('ergebnisse')) || [];
+    // Set variable in local storage
+    // localStorage.setItem('ergebnisse', 'variableValue');
 
-// // Get variable from local storage
-// var variableValue = localStorage.getItem('ergebnisse');
-// ...
+    // // Get variable from local storage
+    // var variableValue = localStorage.getItem('ergebnisse');
+    // ...
 
-// Am Ende des Spiels das Ergebnis ins Array eintragen und im localStorage speichern
-ergebnisse = ergebnisse.reverse().splice(0, 20);
-ergebnisse.push({
-  hand1: hand1,
-  hand2: hand2,
-  hand1Sum: hand1Sum,
-  hand2Sum: hand2Sum,
-  gewinner: (hand2Sum > hand1Sum) ? 'Du' : (hand1Sum > hand2Sum) ? 'Computer' : 'Unentschieden'
-});
+    // Am Ende des Spiels das Ergebnis ins Array eintragen und im localStorage speichern
+    ergebnisse = ergebnisse.reverse(12).splice(0, 12);
+    ergebnisse.unshift({
+      hand1: hand1,
+      hand2: hand2,
+      hand1Sum: hand1Sum,
+      hand2Sum: hand2Sum,
+      gewinner: (hand2Sum > hand1Sum) ? 'Du' : (hand1Sum > hand2Sum) ? 'Computer' : 'Unentschieden'
+    });
 
 
-localStorage.setItem('ergebnisse', JSON.stringify(ergebnisse));
+    localStorage.setItem('ergebnisse', JSON.stringify(ergebnisse));
 
-// ...
+    // ...
 
-// Beim Laden der Seite die Ergebnisse aus dem Array laden und anzeigen
-const ergebnisseEl = document.getElementById("ergebnisse");
-if (ergebnisse.length > 0) {
-  ergebnisseEl.innerHTML = "<h2>Letzte Spiele:</h2>";
-  for (let i = 0; i < ergebnisse.length; i++) {
-    const spiel = ergebnisse[i];
-    ergebnisseEl.innerHTML += "Spiel " + (i + 1) + ": " + spiel.hand2 + " gegen " + spiel.hand1 + ". Ergebnis: " + spiel.hand2Sum + " - " + spiel.hand1Sum + ". Gewinner: " + spiel.gewinner + "<br>";
-  }
-}
-
-}
-function reset() {
-  // Reset the deck array
-  deck = [];
-  for (let i = 0; i < farben.length; i++) {
-      for (let j = 0; j < karte.length; j++) {
-          const wert = karte[j];
-          const mappedWert = werteMapping[wert] || wert;
-          deck.push(farben[i] + ' ' + mappedWert);
+    // Beim Laden der Seite die Ergebnisse aus dem Array laden und anzeigen
+    const ergebnisseEl = document.getElementById("ergebnisse");
+    if (ergebnisse.length > 0) {
+      ergebnisseEl.innerHTML = "<h2>Letzte Spiele:</h2>";
+      for (let i = 0; i < ergebnisse.length; i++) {
+        const spiel = ergebnisse[i];
+        ergebnisseEl.innerHTML += "Spiel " + (i + 1) + ": " + spiel.hand2 + " gegen " + spiel.hand1 + ". Ergebnis: " + spiel.hand2Sum + " - " + spiel.hand1Sum + ". Gewinner: " + spiel.gewinner + "<br>";
       }
+    }
+  }
+  
+  function reset() {
+    // Reset the deck array
+    deck = [];
+    for (let i = 0; i < farben.length; i++) {
+      for (let j = 0; j < karte.length; j++) {
+        const wert = karte[j];
+        const mappedWert = werteMapping[wert] || wert;
+        deck.push(farben[i] + ' ' + mappedWert);
+      }
+    }
+
+    // Reset the hands
+
+    hand1 = [];
+    hand2 = [];
+
+    // Remove the highScores data from local storage
+    localStorage.removeItem('ergebnisse');
+
+    // Clear the high score list
+    const ergebnisse = document.getElementById('ergebnisse');
+    ergebnisse.innerHTML = '';
   }
 
-  // Reset the hands
-  hand1 = [];
-  hand2 = [];
-
-  // Remove the highScores data from local storage
-  localStorage.removeItem('ergebnisse');
-
-  // Clear the high score list
-  const highScoreList = document.getElementById('ergebnisse');
-  highScoreList.innerHTML = '';
-}
-
-const resetBtn = document.createElement('button');
-resetBtn.textContent = 'Zurücksetzen';
-resetBtn.addEventListener('click', reset);
-document.body.appendChild(resetBtn);
+// const resetBtn = document.createElement('button');
+// resetBtn.textContent = 'Zurücksetzen';
+// resetBtn.addEventListener('click', reset);
+// document.body.appendChild(resetBtn);
